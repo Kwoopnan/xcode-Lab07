43f8b79777676da3f5c2acc3110650d650a20141
Kwoopnan <dakwakaskwoopnan@gmail.com>
2025-10-30 14:06:36 -0700

    Initial Commit

---
+++ xcode Lab07/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  xcode Lab07
+//
+//  Created by Kwoopnan Dakwakas on 10/30/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
